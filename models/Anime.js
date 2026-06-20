4f08522d3ed4950c71f889d485d600f5449eee68
Zde12creat <zakdeveloper632@gmail.com>
2026-06-20 09:15:09 +0700

    Create Anime.js

---
+++ models/Anime.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const AnimeSchema = new mongoose.Schema(
+  {
+    _id: String,
+    shortcut: String,
+    title: String,
+    category: String,
+    prompt: String,
+    tags: [String],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Anime", AnimeSchema);
